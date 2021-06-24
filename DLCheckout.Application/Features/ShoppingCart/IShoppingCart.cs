4479463c29c1fbe2840942e75a601e5d24ea2ffd
Anthony Watts <anthonypwatts@gmail.com>
2021-06-24 13:27:24 +0100

    Created solution, generated very basic shopping cart in a new 'Application' project, added a Console App to demo. A VERY simple setup which meets Step 1's requirements.

---
+++ DLCheckout.Application/Features/ShoppingCart/IShoppingCart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DLCheckout.Application.Features.ShoppingCart
+{
+    public interface IShoppingCart
+    {
+        public decimal GetTotal();
+        public IEnumerable<string> GetItems();
+        public void AddItem(string item);
+        public void AddItems(IEnumerable<string> items);
+    }
+}
